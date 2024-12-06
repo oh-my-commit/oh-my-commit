d5e0dce002fd3a8de5067288fd30b52048037b7b
markshawn2020 <shawninjuly@gmail.com>
2024-12-06 12:17:28 +0800

    :sparkles: separate webview

--- webview-ui/src/CommitMessage.tsx
+++ webview-ui/src/CommitMessage.tsx
@@ -36,7 +36,7 @@ const CommitMessage = () => {
       />
       <div className="button-container">
         <vscode-button appearance="secondary" onClick={handleCancel}>
-          Cancel
+          Cancel2
         </vscode-button>
         <vscode-button onClick={handleSubmit}>Commit</vscode-button>
       </div>
