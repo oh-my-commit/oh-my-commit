cee256dd89452e7fdab3591aba7fe6c95f8bd9b7
markshawn2020 <shawninjuly@gmail.com>
2024-12-14 02:40:18 +0800

    refactor: more shared

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,16 @@
+// Types
 export * from './types/git';
 export * from './types/tree-node';
 export * from './types/commit-state';
+
+// Constants
+export * from './constants/git';
+
+// Utils
+export * from './utils/git';
+
+// Config
+export * from './config/validation';
+
+// Events
+export * from './events/git';
