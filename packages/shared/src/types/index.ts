27def688cc7f4a6e5abe5a2af8578180bd3531ff
markshawn2020 <shawninjuly@gmail.com>
2024-12-16 02:04:20 +0800

    fix: webview-ui webpack build

---
+++ packages/shared/src/types/index.ts
@@ -0,0 +1,5 @@
+export * from "./commit";
+export * from "./git";
+export * from "./model";
+export * from "./provider";
+export * from "./tree";
--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/types/index.ts"],
   format: ["cjs", "esm"],
   dts: {
     compilerOptions: {
       composite: false,
-      incremental: false
-    }
+      incremental: false,
+    },
   },
   sourcemap: true,
   clean: true,
