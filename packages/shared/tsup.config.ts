20c611b067ec8b12fc3d1aaedb50f87d59b819e6
markshawn2020 <shawninjuly@gmail.com>
2024-12-20 00:33:49 +0800

    . fix build shared

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -3,9 +3,14 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   entry: ["src/index.ts", "src/common/index.ts", "src/server/index.ts"],
   format: ["esm", "cjs"],
-  dts: true,
+  dts: {
+    compilerOptions: {
+      composite: false,
+      incremental: false,
+      tsBuildInfoFile: undefined
+    }
+  },
   splitting: false,
   sourcemap: true,
   clean: true,
-  onSuccess: "tsc --emitDeclarationOnly --declaration", // 生成类型文件
 })
