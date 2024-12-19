72648768e6af68057547179e6d22006877a4e781
markshawn2020 <shawninjuly@gmail.com>
2024-12-20 00:51:50 +0800

    fix eslint find tsup

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -3,13 +3,7 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   entry: ["src/index.ts", "src/common/index.ts", "src/server/index.ts"],
   format: ["esm", "cjs"],
-  dts: {
-    compilerOptions: {
-      composite: false,
-      incremental: false,
-      tsBuildInfoFile: undefined,
-    },
-  },
+  dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
