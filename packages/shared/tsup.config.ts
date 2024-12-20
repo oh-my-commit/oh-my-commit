1071e959a9b399a52d98f677e5c8b7db78199089
markshawn2020 <shawninjuly@gmail.com>
2024-12-20 11:47:50 +0800

    fix tsup build

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from "tsup"
+import baseConfig from "../../tsup.config"
 
 export default defineConfig({
+  ...baseConfig,
+
   entry: ["src/index.ts", "src/common/index.ts", "src/server/index.ts"],
-  outDir: "../../dist/shared",
-  format: ["esm", "cjs"],
-  dts: {
-    compilerOptions: {
-      composite: false,
-    },
-  },
-  onSuccess: "tsc --emitDeclarationOnly --declaration",
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  treeshake: true,
-  shims: true,
 })
