92b58aaed8f01a34d89c8782f7fd7bf956e662b1
markshawn2020 <shawninjuly@gmail.com>
2024-12-20 15:46:27 +0800

    .

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -3,8 +3,12 @@ import baseConfig from "../../tsup.config"
 
 export default defineConfig({
   ...baseConfig,
-  // dts: true,
-  // onSuccess: "",
+  // shared 必须要 dts 以生成 .d.ts，但无法开 composite
+  dts: {
+    compilerOptions: {
+      composite: false,
+    },
+  },
 
   entry: ["src/index.ts", "src/common/index.ts", "src/server/index.ts"],
 })
