1ed014061931baaa54a4f540a69891d5a991430f
markshawn2020 <shawninjuly@gmail.com>
2024-12-20 11:00:22 +0800

    -- tsc

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -2,16 +2,17 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
   entry: ["src/index.ts", "src/common/index.ts", "src/server/index.ts"],
+  outDir: "../../dist/shared",
   format: ["esm", "cjs"],
   dts: {
     compilerOptions: {
-      moduleResolution: "Node",
       composite: false,
-      incremental: false,
     },
   },
+  onSuccess: "tsc --emitDeclarationOnly --declaration",
   splitting: false,
   sourcemap: true,
   clean: true,
   treeshake: true,
+  shims: true,
 })
