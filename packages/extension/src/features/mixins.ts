b9da2ece7b6e9646bc774b9a311f0d7f5a303461
markshawn2020 <shawninjuly@gmail.com>
2024-12-21 05:19:35 +0800

    fix build

--- packages/extension/src/features/mixins.ts
+++ packages/extension/src/features/mixins.ts
@@ -2,13 +2,19 @@ import { VscodeExtensionLogger } from "@/vscode-adapters"
 import vscode from "vscode" // Constructor type with static members
 
 // Constructor type with static members
-export type Constructor<T = Record<string, never>> = abstract new (...args: any[]) => T
+export type Constructor<T = object> = abstract new (...args: any[]) => T
 
 // Loggable mixin
 export function Loggable<TBase extends Constructor>(Base: TBase) {
   abstract class LoggableClass extends Base {
-    public config = vscode.workspace.getConfiguration()
-    public logger = new VscodeExtensionLogger("host")
+    public config!: vscode.WorkspaceConfiguration
+    public logger!: VscodeExtensionLogger
+
+    constructor(...args: any[]) {
+      super(...args)
+      this.config = vscode.workspace.getConfiguration()
+      this.logger = new VscodeExtensionLogger("host")
+    }
   }
 
   return LoggableClass
