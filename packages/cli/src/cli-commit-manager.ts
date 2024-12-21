21fdbc0c6157893d8aeecfc73cb162e533cf2224
markshawn2020 <shawninjuly@gmail.com>
2024-12-21 17:00:14 +0800

    .

--- packages/cli/src/cli-commit-manager.ts
+++ packages/cli/src/cli-commit-manager.ts
@@ -1,14 +1,16 @@
-import "reflect-metadata"
-
 import { APP_NAME, CommitManager, ConsoleLogger, TOKENS } from "@shared/common"
 import { ProviderRegistry } from "@shared/server"
 import { Container } from "typedi"
 import { CliConfig } from "./cli-config"
 
 // 配置依赖注入
-Container.set(TOKENS.Config, new CliConfig())
-Container.set(TOKENS.Logger, new ConsoleLogger(`${APP_NAME} Cli`))
-Container.set(TOKENS.ProviderManager, new ProviderRegistry(Container.get(TOKENS.Logger)))
+const config = new CliConfig()
+const logger = new ConsoleLogger(`${APP_NAME} Cli`)
+const providerRegistry = new ProviderRegistry(logger)
+
+Container.set(TOKENS.Config, config)
+Container.set(TOKENS.Logger, logger)
+Container.set(TOKENS.ProviderManager, providerRegistry)
 
 // 获取 CommitManager 实例
 export const cliCommitManager = Container.get(CommitManager)
