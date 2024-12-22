c6dacda7153170ccc2d3f841b13140deb5a8c2d6
markshawn2020 <shawninjuly@gmail.com>
2024-12-23 05:10:42 +0800

    == tsup fix before

--- packages/extension/src/extension.ts
+++ packages/extension/src/extension.ts
@@ -1,38 +1,37 @@
 import "reflect-metadata"
 
+import Container from "typedi"
 import * as vscode from "vscode"
 import { VSCODE_TOKENS } from "./vscode-token"
-import Container from "typedi"
+
+console.log({ aaa: "123       " })
 
 export async function activate(context: vscode.ExtensionContext) {
-  
   try {
     Container.set(VSCODE_TOKENS.Context, context)
-  
+
     // const gitService = new VscodeGit()
-  
+
     // Container.set(VSCODE_TOKENS.GitService, gitService)
-  
+
     // // 1. 注册 config
     // Container.set(TOKENS.Config, Container.get(VscodeConfig))
-  
+
     // // 2. 注册 logger 服务
     // Container.set(TOKENS.Logger, Container.get(VscodeLogger))
-  
+
     // // 3. 注册 statusbar
     // Container.set(VSCODE_TOKENS.StatusbarService, Container.get(StatusBarManager))
-  
+
     // // 3. 注册 provider registry (depends logger)
     // Container.set(TOKENS.ProviderManager, Container.get(ProviderRegistry))
-  
+
     // // 4. 获取 CommitManager 实例
     // Container.set(TOKENS.CommitManager, Container.get(CommitManager))
 
     // await app.initialize();
     context.subscriptions.push({ dispose: () => {} })
-    console.log("Extension activated 5")
-
-
+    console.log("Extension activated 5 ")
   } catch (error: unknown) {
     const message = error instanceof Error ? error.message : "Unknown error occurred"
     void vscode.window.showErrorMessage(`Failed to initialize Oh My Commit: ${message}`)
--- packages/extension/tsup.config.ts
+++ packages/extension/tsup.config.ts
@@ -3,15 +3,9 @@ import { defineConfig } from "tsup"
 export default defineConfig({
   entry: ["src/extension.ts"],
   outDir: "../../dist",
-  format: ["cjs"],
-  platform: "node",
-  target: "node16",
-  sourcemap: true,
-  clean: false,
-  minify: false,
-  watch: true,
-  esbuildOptions(options) {
-    options.external = ["vscode"]
-    options.keepNames = true
-  }
+  // platform: "node",
+  // format: ["cjs"],
+  // sourcemap: true,
+  // clean: true,
+  // watch: true,
 })
