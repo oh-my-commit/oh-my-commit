21d4563951d7e0d4a5ac20195081a55f0b484c56
markshawn2020 <shawninjuly@gmail.com>
2024-12-27 01:47:42 +0800

    .

--- packages/shared/src/server/prompt.template.ts
+++ packages/shared/src/server/prompt.template.ts
@@ -6,6 +6,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+
 import { readFileSync } from "fs"
 import Handlebars, { type TemplateDelegate } from "handlebars"
 import { memoize } from "lodash-es"
@@ -40,3 +41,4 @@ export class PromptTemplate {
     return this.getTemplate()(input)
   }
 }
+
